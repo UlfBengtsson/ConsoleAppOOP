03a462f97a4000f223d8c0f8270982a9b5245481
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-11-06 08:54:38 +0100

    Add project files.

---
+++ ConsoleAppOOP/Models/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppOOP.Models
+{
+    public class Car
+    {
+        private static int carCounter = 1;
+
+        public int Id { get; private set; }
+        private string brand;
+        private string modelName;
+        private string regPlate;
+        public string RegPlate
+        {
+            get
+            {
+                return regPlate;
+            }
+            set
+            {
+                if (value.Length == 6)
+                {
+                    regPlate = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Reg.Plate", "6 symbols on reg.plate only");
+                }
+            }
+        }
+        private int wheels;
+
+        public Car(string brand, string model, string regPlate)
+        {
+            Id = carCounter++;
+            wheels = 4;
+
+            this.brand = brand;
+            modelName = model;
+            RegPlate = regPlate;// use our filter
+        }
+
+        public string Info()
+        {
+            return $"Car Id: {Id}\nBrand: {brand}\nModel: {modelName}\nReg.Plate: {RegPlate}";
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {brand} {modelName}";
+        }
+    }
+}
---
+++ ConsoleAppOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleAppOOP.Models;
+using System;
+
+namespace ConsoleAppOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("BMW","525i","SBP704");
+
+            Console.WriteLine(myCar.Info());
+
+            Car ericsCar = new Car("Volvo","745","Skrott");
+
+            Console.WriteLine(ericsCar.ToString());
+        }
+    }
+}

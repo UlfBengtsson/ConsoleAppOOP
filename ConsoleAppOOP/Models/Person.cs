4f0d8712b01cd0e3b2baa26acd69f1b9a053498a
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-11-06 14:23:12 +0100

    Added person with car list owend

---
+++ ConsoleAppOOP/Models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppOOP.Models
+{
+    class Person
+    {
+        private static int idCounter = 0;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+
+        private List<Car> carsOwned;
+
+        public Person(string name)
+        {
+            Id = ++idCounter;
+            Name = name;
+        }
+
+        public void AddCar(Car car)
+        {
+            carsOwned.Add(car);
+        }
+
+        public bool RemoveCar(int id)
+        {
+            foreach (Car aCar in carsOwned)
+            {
+                if (aCar.Id == id)
+                {
+                    carsOwned.Remove(aCar);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
